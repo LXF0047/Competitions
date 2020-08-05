36ed2d6d18c84e064229bdfd77f96da495c88547
liuxiaofei <liuxiaofei@geyecloud.com>
2020-08-05 11:56:44 +0800

    “增加一些特征

--- ScamCall/filter_features.py
+++ ScamCall/filter_features.py
@@ -4,16 +4,7 @@ import pickle
 import prettytable as pt
 
 
-def rule(t):
-    '''
-    总共 6025
-    流量使用月数为0或1 1689
-    拨出+接听不同电话大于100 2513
-    拨出100个电话以上 1811
-    回拨率不为0 4518
-    手机换卡次数大于10 118
-    缺失值
-    '''
+def data(t):
     if t == 'test':
         df_app = load_data('test_app')
         df_voc = load_data('test_voc')
@@ -35,19 +26,35 @@ def rule(t):
         same_call_count = pickle.load(open(xff_path + 'same_call_count_n.pkl', 'rb'))
         same_call_max = pickle.load(open(xff_path + 'same_call_max_n.pkl', 'rb'))
     else:
-        return None
+        return None, None, None, None
+
+    return df_app, df_voc, same_call_count, same_call_max
+
+
+def rule_analysis(t):
+    # 加载数据
+    df_app, df_voc, same_call_count, same_call_max = data(t)
+
     # 加载字典数据
     id2num, num2id = pickle.load(open(xff_path + 'new_dict.pkl', 'rb'))
 
     # 表格打印
     tb = pt.PrettyTable()
-    tb.field_names = ["说明", "Count", "Total"]
+    tb.field_names = ["说明", "Hit", "Total"]
 
     # 流量使用月数为0或1
     month_p = df_app.groupby('phone_no_m')['month_id'].nunique().reset_index(name='month_count')
     flow_01 = month_p[month_p['month_count'] < 2]['phone_no_m'].to_list()
     tb.add_row(["流量使用月数为0或1", len(flow_01), df_app['phone_no_m'].drop_duplicates().shape[0]])
     # print('流量使用月数为0或1', len(flow_01))
+
+    # 只使用了一个月流量
+    flow_1 = month_p[month_p['month_count'] == 1]['phone_no_m'].to_list()
+    tb.add_row(["只使用了一个月流量", len(flow_1), df_app['phone_no_m'].drop_duplicates().shape[0]])
+
+    # 没使用过流量
+    flow_0 = month_p[month_p['month_count'] == 0]['phone_no_m'].to_list()
+    tb.add_row(["没使用过流量", len(flow_0), df_app['phone_no_m'].drop_duplicates().shape[0]])
 
     # 拨出+接听不同电话大于100
     # call_100 = df[df['phone_no_m'].isin(p_phone)].groupby('phone_no_m')[
@@ -91,6 +98,20 @@ def rule(t):
     tb.add_row(["固定通话个数（打过10次以上的）大于200", same_c, len(same_call_count)])
     tb.add_row(["相同号码拨出电话的最多次数大于200", same_cc, len(same_call_max)])
 
+    # 有通话记录的月份数
+    df_voc['start_datetime'] = pd.to_datetime(df_voc['start_datetime'])
+    # df_voc["hour"] = df_voc['start_datetime'].dt.hour
+    # df_voc["day"] = df_voc['start_datetime'].dt.day
+    df_voc["month"] = df_voc['start_datetime'].dt.month
+    call_month = df_voc.groupby('phone_no_m')['month'].nunique().reset_index(name='call_month')
+    call_month_1 = len([x for x in call_month['call_month'] if x == 1])
+    tb.add_row(["通话月份只有1的（可能临时开卡）", call_month_1, call_month.shape[0]])
+
+    # 通话月份数为1且流量月份数也为1
+    call_month_1_phone = call_month[call_month['call_month'] == 1]['phone_no_m'].tolist()
+    both_1 = len(list(set(call_month_1_phone).intersection(set(flow_1))))
+    tb.add_row(["通话月份只为1且流量月数也为1", both_1, call_month.shape[0]])
+
     print(tb)
 
 
@@ -109,32 +130,42 @@ def same_call(df, t):
 
 if __name__ == '__main__':
     '''
-    +---------------------------------------+-------+-------+
-    |                  说明                 | Count | Total |
-    +---------------------------------------+-------+-------+
-    |           流量使用月数为0或1          |  1196 |  1962 |
-    |        拨出+接听不同电话大于150       |  888  |  1892 |
-    |           拨出200个电话以上           |  630  |  1876 |
-    |         主叫100次以上回拨率为0        |  498  |  1038 |
-    |           手机换卡次数大于15          |   52  |  1892 |
-    |            主叫通话时长为0            |   7   |   7   |
-    | 固定通话个数（打过10次以上的）大于100 |   4   |  1892 |
-    |   相同号码拨出电话的最多次数大于100   |  181  |  1892 |
-    +---------------------------------------+-------+-------+
+    >>> 诈骗
+    +---------------------------------------+------+-------+
+    |                  说明                 | Hit  | Total |
+    +---------------------------------------+------+-------+
+    |           流量使用月数为0或1            | 1196 |  1962 |
+    |           只使用了一个月流量            | 690  |  1962 |
+    |              没使用过流量              | 506  |  1962 |
+    |        拨出+接听不同电话大于150          | 888  |  1892 |
+    |           拨出200个电话以上            | 630  |  1876 |
+    |         主叫100次以上回拨率为0          | 498  |  1038 |
+    |           手机换卡次数大于20            |  29  |  1892 |
+    |            主叫通话时长为0              |  7   |   7   |
+    | 固定通话个数（打过10次以上的）大于200      |  1   |  1892 |
+    |   相同号码拨出电话的最多次数大于200        |  78  |  1892 |
+    |    通话月份只有1的（可能临时开卡）        | 742  |  1892 |
+    |      通话月份只为1且流量月数也为1         | 456  |  1892 |
+    +---------------------------------------+------+-------+
     
-    +---------------------------------------+-------+-------+
-    |                  说明                 | Count | Total |
-    +---------------------------------------+-------+-------+
-    |           流量使用月数为0或1          |  493  |  4144 |
-    |        拨出+接听不同电话大于150       |  919  |  4133 |
-    |           拨出200个电话以上           |  244  |  4113 |
-    |         主叫100次以上回拨率为0        |   51  |  774  |
-    |           手机换卡次数大于15          |   2   |  4133 |
-    |            主叫通话时长为0            |   0   |   0   |
-    | 固定通话个数（打过10次以上的）大于100 |   4   |  4133 |
-    |   相同号码拨出电话的最多次数大于100   |  1272 |  4133 |
-    +---------------------------------------+-------+-------+
+    >>> 正常
+    +---------------------------------------+-----+-------+
+    |                  说明                 | Hit | Total |
+    +---------------------------------------+-----+-------+
+    |           流量使用月数为0或1             | 493 |  4144 |
+    |           只使用了一个月流量             | 187 |  4144 |
+    |              没使用过流量               | 306 |  4144 |
+    |        拨出+接听不同电话大于150           | 919 |  4133 |
+    |           拨出200个电话以上              | 244 |  4113 |
+    |         主叫100次以上回拨率为0            |  51 |  774  |
+    |           手机换卡次数大于20              |  1  |  4133 |
+    |            主叫通话时长为0                |  0  |   0   |
+    | 固定通话个数（打过10次以上的）大于200        |  0  |  4133 |
+    |   相同号码拨出电话的最多次数大于200          | 498 |  4133 |
+    |    通话月份只有1的（可能临时开卡）           | 111 |  4133 |
+    |      通话月份只为1且流量月数也为1           |  80 |  4133 |
+    +---------------------------------------+-----+-------+
     '''
-    rule('n')
+    rule_analysis('n')
     print('*' * 100)
-    rule('p')
+    rule_analysis('p')
